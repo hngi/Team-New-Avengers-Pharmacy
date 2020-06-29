0783c2d752031c5a406663e7e428bfd5319cbd6f
Ifeanyi <doc3coder@gmail.com>
2020-06-29 16:08:56 +0100

    cart sidebar

--- variation-four/src/variation-four.js
+++ variation-four/src/variation-four.js
@@ -1,1 +1,9 @@
+/* Set the width of the sidebar to 250px (show it) */
+function openCart() {
+  document.getElementById('mySidepanel').style.width = '100vw';
+}
 
+/* Set the width of the sidebar to 0 (hide it) */
+function closeCart() {
+  document.getElementById('mySidepanel').style.width = '0';
+}
